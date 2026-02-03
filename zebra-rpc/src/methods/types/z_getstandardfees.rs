fcfb9a0bc5a12026d22d7d1622daf0672e38b380
Mark Henderson <henderson.mark@gmail.com>
2026-02-03 13:03:15 -0500

    fix: pub(crate) with getter macro

--- zebra-rpc/src/methods/types/z_getstandardfees.rs
+++ zebra-rpc/src/methods/types/z_getstandardfees.rs
@@ -1,8 +1,13 @@
 //! Types for the `z_getstandardfees` RPC.
 
+use derive_getters::Getters;
+use derive_new::new;
+
 /// A response to a `z_getstandardfees` RPC request.
-#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, Getters, new)]
 pub struct ZGetStandardFeesResponse {
-    pub standard_fee: u64,
-    pub priority_fee: u64,
+    #[getter(copy)]
+    pub(crate) standard_fee: u64,
+    #[getter(copy)]
+    pub(crate) priority_fee: u64,
 }
